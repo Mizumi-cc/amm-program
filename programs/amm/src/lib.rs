3eb2e47f180f03b7fe342a1b209dd38d4e2388cd
Timothy Yirenkyi <t.o.yirenkyi@gmail.com>
2023-08-24 17:35:01 +0000

    add models

--- programs/amm/src/lib.rs
+++ programs/amm/src/lib.rs
@@ -13,3 +13,40 @@ pub mod amm {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq, Debug, Copy)]
+pub struct PoolPair {
+    pub token_one: Pubkey,
+    pub token_one_amount: u64,
+    pub token_two: Pubkey,
+    pub token_two_amount: u64
+}
+
+#[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq, Eq, Debug, Copy)]
+pub enum LPAccountStatus {
+    OPEN,
+    CLOSED
+}
+
+#[account]
+pub struct LPAccount {
+    pub authority: Pubkey,
+    pub lp_token_address: Pubkey,
+    pub lp_pair: PoolPair,
+    pub lp_token_amount: u64,
+    pub status: LPAccountStatus,
+    pub supply_ts: i64,
+    pub updated_ts: i64,
+    pub bump: u8,
+}
+
+#[account]
+pub struct PoolPairAccount {
+    pub lp_token_address: Pubkey,
+    pub lp_pair: PoolPair,
+    pub lp_token_supply: u64,
+    pub name: String,
+    pub created_ts: i64,
+    pub updated_ts: i64,
+    pub bump: u8
+}
